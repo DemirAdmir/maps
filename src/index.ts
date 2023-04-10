381b1d538c8ddfe80ba97209e12ed1c2a4da2cf7
DemirAdmir <admirdemir@gmail.com>
2023-04-10 16:41:39 +0200

    bug fixing - google map

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-import { User } from "./User";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-console.log(user);
+// const user = new User();
+// console.log(user);
+
+// const company = new Company();
+// console.log(company);
+const mapDiv = document.getElementById('map')!;
+let map: google.maps.Map;
+new google.maps.Map(mapDiv as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
+
+
+
