5f429dba0d2686f4a4f40a5b54255afc599d01d5
DemirAdmir <admirdemir@gmail.com>
2023-04-10 21:59:20 +0200

    bug fixing

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,4 +1,21 @@
-const mapDiv = document.getElementById('map')!;
+// const mapDiv = document.getElementById('map')!;
+import * as dotenv from "dotenv";
+dotenv.config();
 
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor() {
+    this.googleMap = new google.maps.Map(
+      document.getElementById('map')!,
+      {
+        zoom: 2,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      });
+  }
+}
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,3 @@
-import * as dotenv from "dotenv";
-import { Loader } from '@googlemaps/js-api-loader';
+import { CustomMap } from './CustomMap';
 
-dotenv.config();
-const mapDiv = document.querySelector('map')!;
-const loader = new Loader({
-  apiKey: "GOOGLE_MAPS_API_KEY",
-  version: "weekly",
-  libraries: ["places"]
-});
-
-const mapOptions = {
-  center: {
-    lat: 0,
-    lng: 0
-  },
-  zoom: 4
-};
-
-loader
-  .load()
-  .then((google) => {
-    new google.maps.Map(mapDiv, mapOptions);
-  })
-  .catch(e => {
-    console.log(e);
-  });
-
-
+new CustomMap();
